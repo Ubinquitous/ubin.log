001842fd381534e2857e0d81f7a846bcce5c614c
Ubinquitous <ubinquitous1@gmail.com>
2023-05-17 00:43:05 +0900

    DELETE : navigation & add search bar

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,25 +1,16 @@
 import React from "react";
 import * as S from "./style";
-
-const navigationLink = [
-  { name: "Github", src: "https://github.com/ubinquitous" },
-  { name: "Instagram", src: "https://instagram.com/ubin._ing" },
-  { name: "Velog", src: "https://velog.io/@ubinquitous" },
-  { name: "Introduce", src: "https://ubinquitous.github.io" },
-];
+import Search from "assets/search.svg";
 
 const Header = () => {
   return (
     <S.HeaderLayout>
       <S.HeaderContainer>
         <S.HeaderTitleText href="/">ubin.log</S.HeaderTitleText>
-        <S.HeaderNavContainer>
-          {navigationLink.map(({ name, src }) => (
-            <S.HeaderNavText key={name} href={src} target="_blank">
-              {name}
-            </S.HeaderNavText>
-          ))}
-        </S.HeaderNavContainer>
+        <S.HeaderSearchContainer>
+          <S.HeaderSearchInput type="text" />
+          <S.HeaderSearchButton src={Search} alt="" />
+        </S.HeaderSearchContainer>
       </S.HeaderContainer>
     </S.HeaderLayout>
   );
