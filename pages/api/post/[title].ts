b386e06eb5b7c25bd089de69ef06d7eac51ad0d5
Ubinquitous <ubinquitous1@gmail.com>
2023-05-16 23:46:39 +0900

    ADD : getOne post api

---
+++ pages/api/post/[title].ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import path from "path";
+import PostType from "@/types/post.type";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<PostType | string>): void {
+  const { title } = req.query;
+
+  if (typeof title === "string") {
+    const filename = title + ".md";
+    const filePath = path.join(process.cwd(), "contents", filename);
+
+    if (fs.existsSync(filePath)) {
+      const fileContent = fs.readFileSync(filePath, "utf-8");
+
+      const response = {
+        name: title,
+        image: fileContent.substring(fileContent.indexOf("(") + 1, fileContent.indexOf(")")),
+        category:
+          fileContent[0] !== "["
+            ? null
+            : fileContent.substring(fileContent.indexOf("[") + 1, fileContent.indexOf("]")).split(", "),
+        date: parseInt(fileContent.substring(fileContent.indexOf("{") + 1, fileContent.indexOf("}"))),
+        contents: fileContent.replace(
+          `[${
+            fileContent[0] !== "["
+              ? null
+              : fileContent.substring(fileContent.indexOf("[") + 1, fileContent.indexOf("]"))
+          }]\n(${fileContent.substring(
+            fileContent.indexOf("(") + 1,
+            fileContent.indexOf(")"),
+          )})\n{${fileContent.substring(fileContent.indexOf("{") + 1, fileContent.indexOf("}"))}}\n\n`,
+          "",
+        ),
+      };
+
+      res.json(response);
+    } else {
+      res.status(404).send("File not found");
+    }
+  } else {
+    res.status(400).send("Invalid query parameter");
+  }
+}
