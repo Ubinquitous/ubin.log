3469b09d2d6722078521c0a33963f91205b51ec4
Ubinquitous <ubinquitous1@gmail.com>
2023-05-17 00:42:26 +0900

    SET : lib require for protect css import error

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const removeImports = require("next-remove-imports")({});
+
+const nextConfig = removeImports({
   reactStrictMode: false,
   images: {
     domains: ["media.discordapp.net"],
   },
-};
+});
 
 module.exports = nextConfig;
