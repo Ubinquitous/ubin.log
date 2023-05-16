7e24af907198c6df22d1f54c26294f341aa2c486
Ubinquitous <ubinquitous1@gmail.com>
2023-05-16 23:47:23 +0900

    ADD : API connecting

--- pages/post/[title].tsx
+++ pages/post/[title].tsx
@@ -1,14 +1,28 @@
-import { useRouter } from "next/router";
+import PostLayout from "@/layouts/PostLayout";
+import DetailPropsType from "@/types/detail.props.type";
+import axios from "axios";
+import { GetServerSideProps } from "next";
 import React from "react";
 
-const Detail = () => {
-  const router = useRouter();
+const Detail = ({ post }: DetailPropsType) => {
+  return <PostLayout {...post} />;
+};
 
-  return (
-    <div>
-      <h1>{router.query.title}</h1>
-    </div>
-  );
+const getApiDocs = async (postname: string) => {
+  try {
+    return (await axios.get(`api/post/${postname}`)).data;
+  } catch (err) {
+    return false;
+  }
+};
+
+export const getServerSideProps: GetServerSideProps<DetailPropsType> = async (context) => {
+  const { params } = context;
+  const post = await getApiDocs(params?.title as string);
+
+  if (!post) return { notFound: true };
+
+  return { props: { post } };
 };
 
 export default Detail;
