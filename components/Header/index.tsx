a601813d10872aae1091e63109ed63c9bd04ff84
Ubinquitous <ubinquitous1@gmail.com>
2023-05-17 11:53:25 +0900

    ADD : 검색 컴포넌트

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import * as S from "./style";
 import Search from "assets/search.svg";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const [search, setSearch] = React.useState("");
+  const router = useRouter();
+  const onNavigateResultPage = (searchvalue: string) => {
+    router.push(`/search//${searchvalue}`);
+  };
+
   return (
     <S.HeaderLayout>
       <S.HeaderContainer>
         <S.HeaderTitleText href="/">ubin.log</S.HeaderTitleText>
         <S.HeaderSearchContainer>
-          <S.HeaderSearchInput type="text" />
-          <S.HeaderSearchButton src={Search} alt="" />
+          <S.HeaderSearchInput type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+          <S.HeaderSearchButton onClick={() => onNavigateResultPage(search)} src={Search} alt="" />
         </S.HeaderSearchContainer>
       </S.HeaderContainer>
     </S.HeaderLayout>
